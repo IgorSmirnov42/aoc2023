6bfa84d7b181f005c3cdf184618098675716c8f8
Igor Smirnov <piter3343@gmail.com>
2023-12-23 09:32:36 +0100

    day23

--- src/day23/p1.kt
+++ src/day23/p1.kt
@@ -2,11 +2,52 @@ package day23
 
 import utils.readInput
 
-private fun solve(input: List<String>) {
+private fun solve(grid: List<String>) {
 
+    fun dfs(x: Int, y: Int, used: MutableSet<Pair<Int, Int>>): Int? {
+        if (x < 0 || y < 0 || x >= grid.size || y >= grid[0].length || grid[x][y] == '#') {
+            return null
+        }
+        if (used.contains(x to y)) {
+            return null
+        }
+        if (x == grid.lastIndex) {
+            return 0
+        }
+        used.add(x to y)
+        val c = grid[x][y]
+        if (c != '.') {
+            val res = when (c) {
+                '>' -> dfs(x, y + 1, used)
+                '<' -> dfs(x, y - 1, used)
+                '^' -> dfs(x - 1, y, used)
+                'v' -> dfs(x + 1, y, used)
+                else -> TODO()
+            }
+            used.remove(x to y)
+            return res?.let { it + 1 }
+        }
+
+        var bestRes: Int? = null
+        for ((shiftX, shiftY) in listOf(0 to 1, 0 to -1, 1 to 0, -1 to 0)) {
+            val res = dfs(x + shiftX, y + shiftY, used)?.let { it + 1 }
+            if (res != null) {
+                bestRes = if (bestRes == null) {
+                    res
+                } else {
+                    maxOf(bestRes, res)
+                }
+            }
+        }
+
+        used.remove(x to y)
+        return bestRes
+    }
+
+    println(dfs(0, grid[0].indexOf('.'), mutableSetOf()))
 }
 
 fun main() {
-    solve(readInput("day23/sample"))
-//    solve(readInput("day23/input"))
+//    solve(readInput("day23/sample"))
+    solve(readInput("day23/input"))
 }
--- src/day23/p2.kt
+++ src/day23/p2.kt
@@ -2,11 +2,56 @@ package day23
 
 import utils.readInput
 
-private fun solve(input: List<String>) {
+private fun solve(grid: List<String>) {
 
+    var globalMax = 0
+    var ctr = 0
+
+    fun dfs(x: Int, y: Int, used: MutableSet<Pair<Int, Int>>, curDist: Int): Int? {
+        if (x == grid.lastIndex) {
+            ctr += 1
+            if (curDist > globalMax) {
+                globalMax = curDist
+                println(globalMax)
+            }
+            return 0
+        }
+        if (ctr >= 10000) {
+            return null
+        }
+        used.add(x to y)
+
+        var bestRes: Int? = null
+        for ((shiftX, shiftY) in listOf(0 to 1, 0 to -1, 1 to 0, -1 to 0).shuffled()) {
+            val newX = x + shiftX
+            val newY = y + shiftY
+            if (newX < 0 || newY < 0 || newX >= grid.size || newY >= grid[0].length || grid[newX][newY] == '#') {
+                continue
+            }
+            if (used.contains(newX to newY)) {
+                continue
+            }
+            val res = dfs(newX, newY, used, curDist + 1)?.let { it + 1 }
+            if (res != null) {
+                bestRes = if (bestRes == null) {
+                    res
+                } else {
+                    maxOf(bestRes, res)
+                }
+            }
+        }
+
+        used.remove(x to y)
+        return bestRes
+    }
+
+    while (true) {
+        ctr = 0
+        dfs(0, grid[0].indexOf('.'), mutableSetOf(), 0)
+    }
 }
 
 fun main() {
-    solve(readInput("day23/sample"))
-//    solve(readInput("day23/input"))
+//    solve(readInput("day23/sample"))
+    solve(readInput("day23/input"))
 }
